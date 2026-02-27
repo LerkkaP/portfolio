1a56ceba8ad1110205a4cb0725ca03919fd01380
Erik Peteri <peteri.erik@gmail.com>
2026-02-27 21:43:26 +0200

    setup old plain html website in nextjs

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,47 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+    <main>
+    <div className="grid"></div>
+    <section className="hero-content">
+      <div className="info">
+        <h1>Erik Peteri</h1>
+        <p>
+          I am currently pursuing a Master’s degree in Data Science at the University of Helsinki, with a strong interest in both the
+          theoretical foundations and practical applications of data science. I am also fascinated by human cognition in all its dimensions, from cognitive processes
+          to the neural mechanisms underlying them.
+        </p>
+        <p>
+          I work as a teaching assistant at the Department of Computer Science in the <a href="https://linkki.cs.helsinki.fi/en/">Linkki Science Lab</a>
+          , a unit focused on early education in computer science and programming. I have also been a long-time instructor
+          in the course <a href="https://hy-tikawe.github.io/materiaali/">Databases and Web Programming</a>.
+        </p>
+        <div className="icons">
+          <a href="https://github.com/LerkkaP">
+            <Image 
+              src="/github.svg"
+              width="40"
+              height="40"
+              alt="Github icon"
             />
-            Deploy Now
           </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
+          <a href="https://linkedin.com/in/erikpeteri">
+            <Image 
+              src="/linkedin.svg"
+              width="40"
+              height="40"
+              alt="Linkedin icon"
+            />        
           </a>
         </div>
-      </main>
-    </div>
+      </div>    
+      <Image 
+        src="/picture.png"
+        width="240"
+        height="240"
+        alt="Picture of Erik"
+      />
+    </section>
+    </main>
   );
 }
